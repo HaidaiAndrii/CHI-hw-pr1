5d9017295fb748c22a936112a142ad8b12e33351
andriihaidai <haidai.andriy@gmail.com>
2021-04-14 15:19:30 +0300

    small fixes

--- src/js/script.js
+++ src/js/script.js
@@ -17,12 +17,13 @@ const form = document.querySelector('.formPart-listener');
 
 
 eye.addEventListener('click', () => {
+
     if (inputPassword.type === 'text') {
         inputPassword.type = 'password';
         eye.style.backgroundImage = "url('../src/img/eye.svg')";
     } else {
         inputPassword.type = 'text';
-        eye.style.backgroundImage = "url('..s/rc/img/closedeye.svg')";
+        eye.style.backgroundImage = "url('../src/img/closedeye.svg')";
     }
 
 }, false);
