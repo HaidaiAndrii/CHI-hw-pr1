7187b6c182caa6caf06d41d69e76e89bf79594ce
andriihaidai <haidai.andriy@gmail.com>
2021-04-14 15:31:53 +0300

    small fixes

--- src/js/script.js
+++ src/js/script.js
@@ -23,6 +23,7 @@ eye.addEventListener('click', () => {
         eye.style.backgroundImage = "url('../src/img/eye.svg')";
     } else {
         inputPassword.type = 'text';
+        eye.style.backgroundImage = "url('../src/img/closedeye.svg')";
     }
 
 });
